8711ce9ab08cb099b7bcf4830d7dc6c08267b587
hhack <hans.f.hack@googlemail.com>
2022-11-16 21:43:55 +0100

    added matomo and snow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useMatomo } from '@lib/hooks/useMatomo'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useMatomo()
   return <Component {...pageProps} />
 }
