55bbf91eaa282886e54ee8a94129bd5665797846
hhack <hans.f.hack@googlemail.com>
2022-11-18 14:46:34 +0100

    first data filter draft

--- src/lib/filterMarkets.js
+++ src/lib/filterMarkets.js
@@ -1,4 +1,25 @@
 // setMarketFilterDate(selected.getMonth() + 1, selected.getDate())
+function toDate(string) {
+  const dateArray = string.split('.')
+  const newString =
+    '20' + dateArray[2] + '-' + dateArray[1] + '-' + dateArray[0]
+  return new Date(newString)
+}
+
+function openOnDate(date, d) {
+  console.log(
+    date >= toDate(d.von) && date <= toDate(d.bis),
+    date,
+    toDate(d.von),
+    toDate(d.bis),
+    d
+  )
+  if (date >= toDate(d.von) && date <= toDate(d.bis)) {
+    return true
+  }
+  return false
+}
+
 export function filterMarkets(
   data,
   marketFilterInternational,
@@ -14,6 +35,10 @@ export function filterMarkets(
       return
     }
     if (marketFilterInternational && d['international'] === '0') {
+      d.inaktiv = true
+      return
+    }
+    if (marketFilterDate && !openOnDate(marketFilterDate, d)) {
       d.inaktiv = true
       return
     }
