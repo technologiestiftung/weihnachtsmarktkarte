28b0d9818e118037370c3c3cb1e22d9337e50aaf
hhack <hans.f.hack@googlemail.com>
2023-11-20 14:45:40 +0100

    fixed sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
   theme: {
     screens: {
       xs: '360px',
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     fontFamily: {
       sans: [
